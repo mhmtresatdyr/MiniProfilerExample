874a69e50f53928e1eacd27308b6ee2b44cf9f47
Mehmet Reşat Duyar <MhmT_ResaT_DyR@hotmail.com>
2020-11-14 20:41:11 +0300

    Add project files.

---
+++ MiniProfilerExample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MiniProfilerExample.Models;
+using StackExchange.Profiling;
+
+namespace MiniProfilerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (MiniProfiler.Current.Step("İlk ViewData Atama"))
+            {
+                ViewData["Test1"] = "Test1 ViewDatası set edildi";
+
+                using (MiniProfiler.Current.Step("İkinci ViewData Atama"))
+                {
+                    ViewData["Test2"] = "Test2 ViewDatası set edildi";
+                    return View();
+                }
+            }
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
